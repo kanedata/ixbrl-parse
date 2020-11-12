cfe815c35e7f4ff958c895c5b6e67244625929e5
David Kane <david@dkane.net>
2020-11-12 10:28:57 +0000

    linting on setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-exec(open('ixbrlparse/version.py').read())
+exec(open("ixbrlparse/version.py").read())
 
 setup(
     name="ixbrlparse",
@@ -16,19 +16,19 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/drkane/ixbrl-parse",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=[
-        'beautifulsoup4',
-        'lxml',
-        'word2number',
+        "beautifulsoup4",
+        "lxml",
+        "word2number",
     ],
 )
